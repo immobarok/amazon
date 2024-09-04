b5fc134a682c95b1488c1647e9ce1bacb920bdaf
immobarok <immobarokali2001@gmail.com>
2024-09-04 20:26:04 +0600

    date and delivery time updated

--- amazon-main/Script/checkout.js
+++ amazon-main/Script/checkout.js
@@ -1,6 +1,17 @@
 import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from './utils/money.js';
+import { dayjs } from 'https://cdn.jsdelivr.net/npm/dayjs@2.0.0-alpha.2/dist/index.mjs';
+
+
+//date summary 
+const today = dayjs();
+const deliveryDate = today.add(7, 'days');
+console.log(deliveryDate.format('dddd, MMMM D'))
+
+
+
+
 
 
 let cartSummaryHtml = '';
--- amazon-main/Script/utils/money.js
+++ amazon-main/Script/utils/money.js
@@ -1,3 +1,7 @@
 export function formatCurrency(priceCents) {
   return (priceCents / 100).toFixed(2);
 }
+
+
+//default 1 time export only 
+export default formatCurrency;
